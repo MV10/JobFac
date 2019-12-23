ca5a4301f0bee2b3c8cee91053ac3657bee441c9
Jon McGuire <mcguirev10@gmail.com>
2019-12-23 12:43:57 -0500

    package updates, add DevConsoleHost

---
+++ DevConsoleHost/Program.cs
@@ -0,0 +1,62 @@
+﻿using JobFac.runtime;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DevConsoleHost
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var host = Host.CreateDefaultBuilder(args);
+
+                host.UseOrleans(builder =>
+                {
+                    builder
+                    
+                    // TODO use real configuration
+                    .UseLocalhostClustering() // cluster and service IDs default to "dev"
+                    
+                    .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                    .ConfigureApplicationParts(parts =>
+                    {
+                        parts.AddApplicationPart(typeof(JobFactory).Assembly).WithReferences();
+                        parts.AddApplicationPart(typeof(Job).Assembly).WithReferences();
+                        parts.AddApplicationPart(typeof(Sequence).Assembly).WithReferences();
+                        //parts.AddApplicationPart(typeof(SchedulerSingleton).Assembly).WithReferences();
+                    });
+                });
+
+                host.ConfigureLogging(builder =>
+                {
+                    builder
+                    .AddFilter("Microsoft", LogLevel.Warning)   // generic host lifecycle messages
+                    .AddFilter("Orleans", LogLevel.Warning)     // suppress status dumps
+                    .AddFilter("Runtime", LogLevel.Warning)     // also an Orleans prefix
+                    .AddDebug()                                 // VS Debug window
+                    .AddConsole();
+                });
+
+                host.ConfigureServices(services =>
+                {
+                    services.AddLogging();
+                });
+
+                await host.RunConsoleAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
