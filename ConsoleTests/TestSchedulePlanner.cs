222700af76cdd2b00328b5b71902a1ceec187815
Jon McGuire <mcguirev10@gmail.com>
2020-01-01 14:01:23 -0500

    sync to fix upper/lowercase mismatch

---
+++ ConsoleTests/TestSchedulePlanner.cs
@@ -0,0 +1,38 @@
+﻿using JobFac.Library.DataModels;
+using JobFac.Services.Scheduling;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleTests
+{
+    public class TestSchedulePlanner : CoordinatedBackgroundService
+    {
+        public TestSchedulePlanner(IHostApplicationLifetime appLifetime)
+            : base(appLifetime)
+        {
+        }
+
+        protected override async Task ExecuteAsync()
+        {
+            try
+            {
+                var job = new DefinitionExternalProcess
+                {
+
+                };
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception {ex}");
+            }
+            finally
+            {
+                Console.WriteLine("Calling StopApplication");
+                appLifetime.StopApplication();
+            }
+
+        }
+    }
+}
